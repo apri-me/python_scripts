836d3dd43a83439d3da67281247fd9b425db769e
aprime <the.aprime2020@gmail.com>
2021-05-04 21:15:42 +0430

    btc price checker

--- btc.check/bitcoin_price.py
+++ btc.check/bitcoin_price.py
@@ -1,2 +1,9 @@
 import requests
 
+url = "https://api.coinex.com/v1/market/ticker"
+market = 'BTCUSDT'
+params = {'market': market}
+r = requests.get(url, params=params)
+if r.json()['code']:
+    raise Exception(r.json()['message'])
+print(r.json()['data']['ticker']['last'])
